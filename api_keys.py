3b294e58112ca530579d9cc54ff40994cb0a8193
Nikki Ransom <95599933+nikdav3126@users.noreply.github.com>
2022-01-30 19:11:37 -0500

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,8 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "a548dce3b402b0be583958bdd81c1b49"
-
-# Google API Key
-g_key = "AIzaSyA3CblITZHVEPl9H1rmZBJAEp1CV5vqlBg"
-
-# Census API Key
-census_key = "bed124b0e0d4c192d399a99e6b297ce2b7c46a53"
